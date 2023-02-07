cc9546f8951988057eca1f1dab5ba3a8f0eaffc5
unknown <ajeetkhan0420@gmail.com>
2023-02-07 19:40:41 +0530

    new file add

---
+++ index.js
@@ -0,0 +1,28 @@
+// let ans = Number(prompt("How many time do you want too print Hello world"))
+// for(let i = 1 ;i <= ans;i++){
+//     console.log("hello world");
+// }
+
+// 1 to 100 tk calclut
+// let count = 0
+// for(let i = 0;i<=100 ; i++){
+//     if(i%2==1){
+//     count = count + i
+//     }
+// }
+// console.log(count);
+
+//  12481632
+// 11
+// for(let i = 0;i<=4050;i++){
+//        let m = i 
+//           i = i + i 
+//       console.log(m+1);
+
+// }
+
+// for(let i = 0;i <= 33;i *= 2){
+// console.log(i);
+// }
+
+
